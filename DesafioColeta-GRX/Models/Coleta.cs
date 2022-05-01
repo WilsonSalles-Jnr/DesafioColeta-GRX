cdc2693cf123a7ade9dab6db06f4372058188a8b
Wilson Salles Junior <64506476+WilsonSalles-Jnr@users.noreply.github.com>
2022-05-01 17:48:01 -0300

    feat: Criando model de coleta

---
+++ DesafioColeta-GRX/Models/Coleta.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace DesafioColeta_GRX.Models
+{
+    public class Coleta
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public bool Pergunta1 { get; set; }
+        [Required]
+        public bool Pergunta2 { get; set; }
+        [StringLength(10)]
+        [Required]
+        public string Pergunta3 { get; set; }
+        [StringLength(200)]
+        [Required]
+        public string Pergunta4 { get; set; }
+        [Required]
+        public int QuantidadePositiva { get; set; }
+        [Required]
+        public int QuantidadeNegativa { get; set; }
+        [Required]
+        public int QuantidadeNaoAvaliada { get; set; }
+
+    }
+}
