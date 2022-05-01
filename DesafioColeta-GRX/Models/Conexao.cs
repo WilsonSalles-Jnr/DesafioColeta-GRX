3d75452c94b515f1396ea8628e41698a2f17b560
Wilson Salles Junior <64506476+WilsonSalles-Jnr@users.noreply.github.com>
2022-05-01 18:09:12 -0300

    feat: migrations executada e funcionando

---
+++ DesafioColeta-GRX/Models/Conexao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DesafioColeta_GRX.Models
+{
+    public class Conexao : DbContext
+    {
+        public Conexao() : base("ColetaDB")
+        {
+
+        }
+        public DbSet<Coleta> Coleta { get; set; }
+    }
+}
