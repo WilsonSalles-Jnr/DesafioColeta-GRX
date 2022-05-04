9d9c3968c3bf01d7886cc9c1e87ea73186b0fbde
Wilson Salles Junior <64506476+WilsonSalles-Jnr@users.noreply.github.com>
2022-05-03 22:14:06 -0300

    feat: retornando informações de formulário

--- DesafioColeta-GRX/Scripts/counter.js
+++ DesafioColeta-GRX/Scripts/counter.js
@@ -15,3 +15,37 @@ $('#text').keyup(function () {
     }
     $('#contador').html(text_length + ' / ' + text_max);
 });
+
+$('#submit').click((e) => {
+    e.preventDefault();
+    const Pergunta1 = $('input[name=logica]:checked', '#formulario').val()
+    const Pergunta2 = $('input[name=desafio]:checked', '#formulario').val()
+    const Pergunta3 = $('#joinGRX').val()
+    const Pergunta4 = $('#text').val()
+    let QuantidadePositiva = 0;
+    let QuantidadeNegativa = 0;
+    let QuantidadeNaoAvaliativa = 0;
+
+    Pergunta1 === 'Sim' ? QuantidadePositiva += 1 : QuantidadeNegativa += 1;
+    Pergunta2 === 'Sim' ? QuantidadePositiva += 1 : QuantidadeNegativa += 1;
+
+    switch (Pergunta3) {
+        case "Sim":
+            QuantidadePositiva += 1;
+            break
+        case "Não":
+            QuantidadeNegativa += 1;
+            break;
+        case "Não sei":
+            QuantidadeNaoAvaliativa += 1;
+            break;
+        case "Agora":
+            QuantidadePositiva += 2;
+            break;
+        default:
+            null;
+    }
+
+    const retorno = { Pergunta1, Pergunta2, Pergunta3, Pergunta4, QuantidadePositiva, QuantidadeNegativa, QuantidadeNaoAvaliativa };
+    console.log(retorno);
+})
