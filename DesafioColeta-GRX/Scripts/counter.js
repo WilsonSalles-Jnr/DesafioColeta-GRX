b7821f90f0b35a9343171c1586987308f92f41ae
Wilson Salles Junior <64506476+WilsonSalles-Jnr@users.noreply.github.com>
2022-05-03 21:13:35 -0300

    feat: inserindo script de contador

---
+++ DesafioColeta-GRX/Scripts/counter.js
@@ -0,0 +1,17 @@
+﻿const text_max = 200;
+$('#contador').html('0 / ' + text_max);
+$('#text').keyup(function () {
+    const text_length = $('#text').val().length;
+    const text_remaining = text_max - text_length;
+    if (text_length >= 15) {
+        $('#contador').removeClass("bg-red");
+        $('#contador').addClass("bg-green");
+        $('#submit').prop('disabled', false)
+    }
+    if (text_length < 15) {
+        $('#contador').removeClass("bg-green");
+        $('#contador').addClass("bg-red");
+        $('#submit').prop('disabled', true)
+    }
+    $('#contador').html(text_length + ' / ' + text_max);
+});
